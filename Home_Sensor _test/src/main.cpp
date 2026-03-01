6217694c92525cd96bad2ed34091df6f7b52981b
tl3672s <anh.le@alumni.fh-aachen.de>
2026-03-01 01:32:42 +0100

    OLED Test

--- Home_Sensor _test/src/main.cpp
+++ Home_Sensor _test/src/main.cpp
@@ -1,8 +1,21 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
 
+#include <U8g2lib.h>
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define OLED_SDA 1
+#define OLED_SCL 2
+#define OLED_ADDR 0x3C   // meistens 0x3C
+
+//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 volatile bool pressed = false;
+bool displayOn = false;
 
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
 void IRAM_ATTR isr() {
   pressed=true;
 }
@@ -17,6 +30,11 @@ void setup() {
 
   pinMode(0, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(0), isr, RISING);
+  Wire.begin(1, 2);
+
+  u8g2.begin();
+  u8g2.clearBuffer();
+  u8g2.sendBuffer();
 }
 
 void loop() {
@@ -27,6 +45,15 @@ void loop() {
     digitalWrite(3,  !digitalRead(3));
     digitalWrite(18, !digitalRead(18));
     pressed=false;
+
+
+    displayOn = !displayOn;
+    u8g2.clearBuffer();
+    if (displayOn) {
+      u8g2.setFont(u8g2_font_ncenB14_tr);
+      u8g2.drawStr(10, 30, "Hallo Karola");
+    }
+    u8g2.sendBuffer(); 
   }
   
 }
